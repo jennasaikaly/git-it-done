74cd1030f79dda08994edfd55d5dbf0acc27a990
jennasaikaly <jen.saikaly@gmail.com>
2022-04-02 09:45:03 -0400

    added functionality to search form

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,26 @@
-var getUserRepos = function(jennasaikaly) {
+//form variables
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+
+//Adds functionality to search form
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  // get value from input element
+var username = nameInputEl.value.trim();  
+
+if (username) {   //checks that there is a value in that username variable
+  getUserRepos(username); //passes the username value data to the getUserRepos function as an argument
+  nameInputEl.value = ""; //to clear the form, we clear out the input element's value
+} else {
+  alert("Please enter a GitHub username");
+}
+};
+
+//request and receive data from server
+var getUserRepos = function(username) {
   // format the github api url
-  var apiUrl = "https://api.github.com/users/" + "jennasaikaly" + "/repos";
+  var apiUrl = "https://api.github.com/users/" + username + "/repos";
 
   // make a request to the url
   fetch(apiUrl).then(function(response) {
@@ -9,4 +29,5 @@ var getUserRepos = function(jennasaikaly) {
     });
   });
 };
-  getUserRepos();
+userFormEl.addEventListener("submit", formSubmitHandler);
+
